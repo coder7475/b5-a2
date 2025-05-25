fe44a40d771adb13537a8f7d8e8ad07158ff2627
coder7475 <robiulhossain7475@gmail.com>
2025-05-25 18:04:57 +0600

    feat: Add Ranger table to conservation database schema

--- wildlife_monitoring.sql
+++ wildlife_monitoring.sql
@@ -1,1 +1,11 @@
+-- Create Database
 CREATE DATABASE conservation_db;
+
+-- Create Tables for the database
+-- Create Ranger table
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    region TEXT NOT NULL
+);
+
