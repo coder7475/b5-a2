d0c0c7d5ff8b50b9ae94c00164da19bff1cf2fb8
coder7475 <robiulhossain7475@gmail.com>
2025-05-25 18:10:12 +0600

    feat: Add species table to conservation database schema

--- wildlife_monitoring.sql
+++ wildlife_monitoring.sql
@@ -1,7 +1,7 @@
 -- Create Database
 CREATE DATABASE conservation_db;
 
--- Create Tables for the database
+-- ? Create Tables for the database
 -- Create Ranger table
 CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY,
@@ -9,3 +9,11 @@ CREATE TABLE rangers (
     region TEXT NOT NULL
 );
 
+-- Create species table
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name TEXT NOT NULL,
+    scientific_name TEXT NOT NULL,
+    discover_date DATE NOT NULL,
+    conservation_status TEXT NOT NULL
+);
